a103c56f0b6894bfeb4a9de581e8cdb046ae9f54
Don Ho <don.ho@outlook.de>
2021-10-16 14:40:00 +0200

    Add simple REST call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Col, Container, Row} from "react-bootstrap";
+
+type Rating = {
+    helpful: boolean;
+    comment: string;
+    referenceId: string;
+    link: string;
+}
+
+const Rating: React.FC<{ rating: Rating }> = ({rating}) => {
+    return <div>{rating.comment}</div>
+}
+
+const Ratings: React.FC = () => {
+    const [ratings, setRatings] = React.useState<Rating[] | undefined>(undefined);
+
+    React.useEffect(() => {
+            fetch("http://localhost:8080/doc/1/ratings", {
+                method: 'GET',
+                mode: 'cors'
+            })
+                .then(result => result.json())
+                .then(ratings => setRatings(ratings));
+        }
+        , []);
+
+    if (ratings === undefined) return <div>Keine Daten</div>;
+
+    return <>
+        {ratings.map(rating => <Rating rating={rating}/>)}
+    </>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Ratings/>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
